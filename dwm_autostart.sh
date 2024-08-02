b36367905a82714770f297982117f0db204997f4
Nicklas Rudolfsson <66663564+nirucon@users.noreply.github.com>
2024-08-02 07:08:10 +0200

    Update dwm_autostart.sh

--- dwm_autostart.sh
+++ dwm_autostart.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-# If using dwm.desktop
+# If using login manager and dwm.desktop
 # Place this script in ~/.dwm/ and ensure it's executable
 
 # Start dwm statusbar
-~/.config/suckless/bar-dwm/bar_dwm.sh &
+~/.config/suckless/nirubar-dwm/nirubar-dwm &
 
 # Start compositor
 picom -f &
